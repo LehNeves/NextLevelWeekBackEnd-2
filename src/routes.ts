d4ba44ac483e4b06533d58a3e74251c4783419ff
LehNeves <lneves997@gmail.com>
2020-08-05 23:28:56 -0300

    Terminando segundo dia da NLW, criado 4 rotas já.

--- src/routes.ts
+++ src/routes.ts
@@ -1,62 +1,15 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinute from './utils/convertHourToMinute';
-
+import ClassesController from './controllers/ClassesController';
+import ConnectionsControllers from './controllers/ConnectionsController';
 
 const routes = express.Router();	
+const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsControllers();
 
-interface ScheduleItem {
-	week_day : number;
-	from : string;
-	to : string;
-}
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
-routes.post('/classes', async (request, response) => {
-	const {
-		name,
-		avatar,
-		whatsapp,
-		bio,
-		subject,
-		cost,
-		schedule,
-	} = request.body;
-
-	const insertedUsersIds = await db('users').insert({
-		name,
-		avatar,
-		whatsapp,
-		bio
-	});
-
-	const user_id = insertedUsersIds[0];
-
-	const insertedClassesIds = await db('classes').insert({
-		subject,
-		cost,
-		user_id
-	});
-
-	const class_id = insertedClassesIds[0];
-
-	await db('classes').insert({
-		subject,
-		cost,
-		user_id
-	});
-
-	const classSchedule = schedule.map((scheduleItem : ScheduleItem) => {
-		return {
-			class_id,
-			week_day : scheduleItem.week_day,
-			from : convertHourToMinute(scheduleItem.from),
-			to : convertHourToMinute(scheduleItem.to),
-		}
-	});
-
-	await db("classes_schedule").insert(classSchedule);
-
-	return response.send();
-});
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
 export default routes;
